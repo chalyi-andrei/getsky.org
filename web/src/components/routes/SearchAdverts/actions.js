aaa2bb6d71e4a62d3ef9451100a51f7a11d32f06
andrei <andrey.chaluy@applikey.biz>
2018-06-08 13:19:35 +0300

    add unit tests for SearchAdverts actions

--- web/src/components/routes/SearchAdverts/actions.js
+++ web/src/components/routes/SearchAdverts/actions.js
@@ -1,22 +1,29 @@
+import get from 'lodash/get';
 import { searchAllAdverts } from '../../../api';
 
-export const SEARCH_ADVERTS_REQUEST = 'SEARCH_ADVERTS_REQUEST';
-export const SEARCH_ADVERTS_RESPONSE = 'SEARCH_ADVERTS_RESPONSE';
-export const SET_FILTERS = 'SET_FILTERS';
-export const CLEAR_FILTERS = 'CLEAR_FILTERS';
+export const LOAD_SEARCH_ADVERTS = 'LOAD_SEARCH_ADVERTS';
+export const SEARCH_ADVERTS_SUCCESS = 'SEARCH_ADVERTS_RESPONSE';
+export const SEARCH_ADVERTS_FAILED = 'SEARCH_ADVERTS_FAILED';
 
 export const searchAdverts = query => async (dispatch) => {
-    dispatch({ type: SEARCH_ADVERTS_REQUEST });
+    dispatch({ type: LOAD_SEARCH_ADVERTS });
 
-    const allAdverts = await searchAllAdverts(query);
-
-    dispatch({ type: SEARCH_ADVERTS_RESPONSE, allAdverts });
+    try {
+        const allAdverts = await searchAllAdverts(query);
+        dispatch({ type: SEARCH_ADVERTS_SUCCESS, allAdverts });
+    } catch (err) {
+        const error = get(err, 'response.data', 'error');
+        dispatch({ type: SEARCH_ADVERTS_FAILED, error });
+    }
 };
 
+
+export const SET_FILTERS = 'SET_FILTERS';
 export const setFilters = filters => async dispatch => {
     dispatch({ type: SET_FILTERS, filters });
 };
 
-export const clearFilters = () => dispatch => {
+export const CLEAR_FILTERS = 'CLEAR_FILTERS';
+export const clearFilters = () => async dispatch => {
     dispatch({ type: CLEAR_FILTERS });
 };
